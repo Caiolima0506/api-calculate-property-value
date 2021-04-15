cb06ff802ddb8bbebf83e29d45701a49712d8ffa
Caio Lima <40145864+Caiolima0506@users.noreply.github.com>
2021-04-15 12:55:24 -0300

    excluir dist

--- dist/domain/helpers/MSPropertyValueConfig.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MSPropertyValueConfig = void 0;
-class MSPropertyValueConfig {
-}
-exports.MSPropertyValueConfig = MSPropertyValueConfig;
--- dist/domain/helpers/ResCalculateValueController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResCalculateValueController = void 0;
-class ResCalculateValueController {
-}
-exports.ResCalculateValueController = ResCalculateValueController;
